6990900f2eeeffa2584c703b206080726d60341f
Max Beining <m.beining@outlook.de>
2024-12-20 14:31:36 +0100

    Update path for input_pdbs
    Wrong path (pdb_trimmed) )was used for creating the fasta for all sequences. Bc user should input their pdbs into the "input_pdb" folder I replace this path

--- mpnn_retrain_docu/scripts/3_clustering.sh
+++ mpnn_retrain_docu/scripts/3_clustering.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+mkdir -p ../cluster_data
+
 # create fasta file
 python3 -W ignore ../scripts/get_multifasta_from_pdb_path.py \
-	-p ../pdb_trimmed/ -c A -o ../cluster_data/pdb_trimmed_seqs.fasta
+	-p ../input_pdb/ -c A -o ../cluster_data/input_pdb_seqs.fasta
 
 # Clustering the fasta sequences
 echo "Start clustering"
-mmseqs easy-cluster ../cluster_data/pdb_trimmed_seqs.fasta ../cluster_data/pdb_trimmed_seqs ./tmp \
+mmseqs easy-cluster ../cluster_data/input_pdb_seqs.fasta ../cluster_data/input_pdb_seqs ./tmp \
  	--min-seq-id 0.3 -c 0.8 --cov-mode 0
