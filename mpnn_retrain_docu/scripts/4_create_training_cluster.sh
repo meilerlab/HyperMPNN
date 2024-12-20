08d790325260fd4f1643d301c24ac5e1b0921531
Max Beining <m.beining@outlook.de>
2024-12-20 14:33:08 +0100

    Replace with correct cluster.tsv file
    Bc in script 3_clustering.sh the output cluster should now be names input_pdb_seqs_cluster.tsv

--- mpnn_retrain_docu/scripts/4_create_training_cluster.sh
+++ mpnn_retrain_docu/scripts/4_create_training_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ./create_cluster_lists.py ../pdb_pt/ ../cluster_data/myinput_cluster.tsv
+python3 ./create_cluster_lists.py ../pdb_pt/ ../cluster_data/input_pdb_seqs_cluster.tsv
